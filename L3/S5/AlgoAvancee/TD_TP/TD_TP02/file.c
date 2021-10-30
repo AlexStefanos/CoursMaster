a66d880278a48dd6d60fd393f63b8bbc3481d511
AlexStefanos <alexandrestefanoss@gmail.com>
2021-10-30 14:47:04 +0200

    30/10/21 v3

--- L3/S5/AlgoAvancee/TD_TP/TD_TP02/file.c
+++ L3/S5/AlgoAvancee/TD_TP/TD_TP02/file.c
@@ -1,95 +1,61 @@
-/*
- * file.c
- * Auteur : Leonard Namolaru
- */
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
 #define FILE
 #include "file.h"
 
-/* t_file* createFile()
- * Alloue l'espace mémoire pour une file, remplit les champs de la structure t_file.
- * Retourne un pointeur sur la file.
- */
-t_file* createFile() {
-	t_file* ptrFile = NULL;
-	ptrFile = (t_file*) malloc( sizeof(t_file) );
-	
-	if(!ptrFile){
-		fprintf(stderr, "La fonction malloc() a échoué: espace mémoire insuffisant. \n");
-		exit(EXIT_FAILURE);
-	}
-			
-	ptrFile -> premier = NULL;
-	return ptrFile;	
-} // createFile()
+t_file_head *createFile() {
+    t_file_head *ptrFile;
 
-/* t_file_element* NouvElement(int etiquetteElement)
- * Alloue l'espace mémoire pour un element, remplit les champs de la structure t_file_element.
- * Retourne un pointeur sur lelement
- */
-t_file_element* NouvElement(int etiquetteElement){
-	t_file_element* ptrElement = NULL;
-	ptrElement = (t_file_element*) malloc( sizeof(t_file_element) );
-	
-	if(!ptrElement){
-		fprintf(stderr, "La fonction malloc() a échoué: espace mémoire insuffisant. \n");
-		exit(EXIT_FAILURE);
-	}
-			
-	ptrElement -> precedent = NULL; // Pointeur vers lelement précédent de la file.
-	ptrElement -> suivant = NULL; // Pointeur vers lelement suivant de la file.
-	ptrElement -> etiquette =  etiquetteElement; 
-	
-	return ptrElement;
-} //NouvElement()
+    ptrFile = malloc(sizeof(t_file_head));
+    if(!ptrFile) {
+        fprintf(stderr, "Echec lors de l'allocation de l'espace mémoire (malloc)\n");
+        exit(EXIT_FAILURE);
+    }
+    ptrFile->first = NULL;
+    return(ptrFile);
+}
 
-/* void enfiler(t_file_element* file_element, t_file* file)
- * Permet denfiler un element a la fin de la file
- */
-void enfiler(t_file_element* file_element, t_file* file){
-	if( est_vide(file) )
-	{
-		// Si la file est vide, l'élément devient le premier élément de la file.
-		file -> premier = file_element;
-	}
-	else // Si la file est PAS vide
-	{
-		t_file_element* elementActuel = file -> premier;
-		t_file_element* dernierePosition;		
+t_file_body *newElement(int labelElement) {
+    t_file_body *ptrElement;
 
-		// Le but est d'atteindre la fin de la file 
-		// Chaque fois que nous passons à l'élément suivant, nous approchons de la fin de la file.
-		while(elementActuel != NULL){
-			dernierePosition = elementActuel;
-			elementActuel = elementActuel -> suivant; // Pointeur vers lelement suivant de la file.
-		} // while
-	
-		// et puis d'ajouter le nouvel élément à la fin de la file
-		dernierePosition -> suivant = file_element;
-		file_element -> precedent  = dernierePosition;
-	}	
-} // enfiler()
+    ptrElement = malloc(sizeof(t_file_body));
+    if(!ptrElement) {
+        fprintf(stderr, "Echec lors de l'allocation de l'espace mémoire (malloc)\n");
+        exit(EXIT_FAILURE);
+    }
+    ptrElement->prev = NULL;
+    ptrElement->next = NULL;
+    ptrElement->label = labelElement;
+}
 
+void thread(t_file_body *file_body, t_file_head *file_head) {
+    if(isEmpty(file_head))
+        file_head->first = file_body;
+    else {
+        t_file_body *currentElement = file_head->first;
+        t_file_body *lastPosition;
 
-/* int defiler(t_file* file)
- * Permet de defiler le 1er element de la file.
- * La fonction renvoie la valeur de l'etiquette de l'élément supprimé ou -1 si la file est vide.
- */
-int defiler(t_file* file) {
-	int x = -1; // La valeur à retourné si la file est vide
-	if( !est_vide(file) ) {
-		x = (file -> premier) -> etiquette; // l'etiquette de l'élément.
-		t_file_element* ancienPremier = file -> premier;
-		file -> premier = (file -> premier) -> suivant; // Chaque fois que nous passons à l'élément suivant, nous approchons de la fin de la file.
-		free(ancienPremier); // Libérez l'espace mémoire de l'élément supprimé
-	}
-	return x; // La fonction renvoie la valeur de l'etiquette de l'élément supprimé
-} // defiler()
+        while(currentElement) {
+            lastPosition->next = file_body;
+            file_body->prev = lastPosition;
+        }
+    }
+}
 
-/* int est_vide(t_file* file)
- * La fonction renvoie 1 si la file est vide, 0 sinon.
- */
-int est_vide(t_file* file){
-	return ( ( (file -> premier) == NULL ) ? 1 : 0 );
-} //est_vide()
+int scroll(t_file_head *file_head) {
+    int tmp = -1;
+    if(isEmpty(file_head)) {
+        tmp = (file_head->first)->label;
+        t_file_body *lastFirst = file_head->first;
+        file_head->first = (file_head->first)->next;
+        free(lastFirst);
+    }
+    return(tmp);
+}
+
+int isEmpty(t_file_head *file_head) {
+    if((file_head->first) == NULL)
+        return(1);
+    else
+        return(0);
+}
--- L3/S5/AlgoAvancee/TD_TP/TD_TP02/file.h
+++ L3/S5/AlgoAvancee/TD_TP/TD_TP02/file.h
@@ -1,51 +1,26 @@
-/*
- * file.h
- * Auteur : Leonard Namolaru
- * Ajout dexplications à partir du cours Programmation Impérative / F. Cloppet 
- */
+#ifndef FILE_HEADER
 
-/* Pour éviter les inclusions multiples   
- * Placer le contenu du fichier .h à l’intérieur d’une inclusion conditionnelle
- */
+    #define FILE_HEADER
+    typedef struct t_file_body {
+        struct t_file *prev;
+        struct t_file *next;
+        int label;
+    }t_file_body;
 
-#ifndef FILE_HEADER //if not def
-//Permet de tester si un symbole n'a pas déja été définie par un #define
+    typedef struct t_file_head {
+        struct t_file *first;
+    }t_file_head;
 
-/* l'expression FILE_HEADER  vaut
- * 0 si le fichier n'a pas été inclus
- * 1 s'il a déjà été inclus
- */
- 
-	#define FILE_HEADER
-		
-	typedef struct t_file_element{
-		// Chaque fois que nous passons à l'élément suivant, nous approchons de la fin de la file.
-		struct t_file_element* precedent; // Pointeur vers lelement précédent de la file.
-		struct t_file_element* suivant; // Pointeur vers lelement suivant de la file.
-		int etiquette;
-	} t_file_element ;
+    #ifndef FILE
+        #define EXTERN extern
+    #else
+        #define EXTERN
+    #endif
 
-	typedef struct t_file{
-		struct t_file_element* premier; // Pointeur vers le 1er element de la file.
-	} t_file ;
-	
-	
-	/* Si nous incluons ce fichier dans le fichier plusCourtChemin.c, 
-	 * il est nécessaire d'ajouter le mot extern avant chaque fonction. 
-	 * Cependant, lorsque nous incluons ce fichier dans le fichier file.c, 
-	 * il n'est pas nécessaire d'ajouter le mot extern.
-	*/
-	#ifndef FILE //if not def
-		#define WHERE_FILE extern
-	#else
-		#define WHERE_FILE
-	#endif
-	
-	/* Prototypage des fonctions */
-	WHERE_FILE t_file* createFile();
-	WHERE_FILE t_file_element* NouvElement(int etiquetteElement);
-	WHERE_FILE void enfiler(t_file_element* file_element, t_file* file);
-	WHERE_FILE int defiler(t_file* file);
-	WHERE_FILE int est_vide(t_file* file);	
-	
+    EXTERN t_file_head *createFile();
+    EXTERN t_file_body *newElement(int labelElement);
+    EXTERN void thread(t_file_body *file_body, t_file_head *file_head);
+    EXTERN int scroll(t_file_head *file_head);
+    EXTERN int isEmpty(t_file_head *file_head);
+
 #endif

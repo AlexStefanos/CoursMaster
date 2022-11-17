e14d20e07a0c4913dbab06a036b95419ddcb529c
AlexStefanos <alexandrestefanoss@gmail.com>
2022-11-17 11:11:41 +0100

    17/11/22

--- Master/M1/S1/ProgrammationMultiParadigmes/TP/WorkspaceScala/.idea/sonarlint/issuestore/index.pb
+++ Master/M1/S1/ProgrammationMultiParadigmes/TP/WorkspaceScala/.idea/sonarlint/issuestore/index.pb
@@ -1,5 +1,3 @@
 
-S
-#src/TP04/main/scala/Anagramme.scala,3/9/392472c13ceb2a4a165e1ca5dd6b02273df4dbee
-N
-src/TP05/main/scala/Tp05.scala,0/d/0d2fa37798caeb3a121f8c4b7ae2670713777891
+I
+src/main/scala/tp05.scala,5/7/5743241a4613be20286f22ac84fc1b99b0ee1a42
---
+++ Master/M1/S1/ProgrammationMultiParadigmes/TP/WorkspaceScala/src/main/scala/tmp/Anagramme.scala
@@ -0,0 +1,47 @@
+package tmp
+
+import scala.collection.mutable.HashSet
+
+object Anagramme {
+  def main(args: Array[String]): Unit = {
+    println("Entrez le mot dont vous souhaitez trouver les anagrammes")
+    val search = scala.io.StdIn.readLine()
+    val result = trouveAnagrammesRec(search)
+    println("Nombre de mots : " + result.size)
+    println("Afficher tous les nombres (O/n)")
+    val display = scala.io.StdIn.readChar()
+    var verif = false
+    while (verif == false) {
+      if (display == 'O') {
+        verif = true
+        println("Mots : ")
+        for (tmp <- result) {
+          println(tmp)
+        }
+      }
+      if (display == 'n') {
+        return 0
+      }
+    }
+    return 0
+  }
+
+  def trouveAnagrammesRec(word_ : String): HashSet[String] = {
+    var result: HashSet[String] = new HashSet[String]()
+    var nb = 0
+    for (c <- word_) {
+      var stringBuilder = new StringBuilder(word_)
+      var rest = stringBuilder.deleteCharAt(nb).toString()
+      if (rest.length < 1) {
+        result += s"$c$rest"
+        return result
+      }
+      var rets = trouveAnagrammesRec(rest)
+      for (tmp <- rets) {
+        result += s"$c$tmp"
+      }
+      nb += 1
+    }
+    return result
+  }
+}

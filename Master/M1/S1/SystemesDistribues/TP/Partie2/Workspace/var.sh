ccaec2ae03b02c6adea4bc01099f7603b1e61614
AlexStefanos <alexandrestefanoss@gmail.com>
2022-11-30 21:24:44 +0100

    30/11/22

--- Master/M1/S1/SystemesDistribues/TP/Partie2/Workspace/var.sh
+++ Master/M1/S1/SystemesDistribues/TP/Partie2/Workspace/var.sh
@@ -1,3 +1,3 @@
-export OPENJMS_HOME="Cours/Master/M1/S1/SystemesDistribues/TP/Partie2/Workspace/openjms-0.7.7-beta-1/openjms-0.7.7-beta-1.jar"
-export CLASSPATH=""
-export JAVA_HOME=""
+export JAVA_HOME="/lib/jvm/java-11-openjdk-amd64"
+export OPENJMS_HOME="$HOME/Cours/Master/M1/S1/SystemesDistribues/TP/Partie2/Workspace/openjms-0.7.7-beta-1"
+export CLASSPATH="$OPENJMS_HOME/lib/openjms-0.7.7-beta-1.jar:$OPENJMS_HOME/lib/jms-1.1.jar:$OPENJMS_HOME/lib/jndi-1.2.1.jar:$HOME/Cours/Master/M1/S1/SystemesDistribues/TP/Partie2/Workspace/tp01"

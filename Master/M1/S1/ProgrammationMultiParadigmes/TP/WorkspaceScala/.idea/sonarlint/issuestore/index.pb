0c30a31578c45204a8dfb01bd19357837d75782d
AlexStefanos <alexandrestefanoss@gmail.com>
2023-01-22 16:35:51 +0100

    22/01/23

--- Master/M1/S1/ProgrammationMultiParadigmes/TP/WorkspaceScala/.idea/sonarlint/issuestore/index.pb
+++ Master/M1/S1/ProgrammationMultiParadigmes/TP/WorkspaceScala/.idea/sonarlint/issuestore/index.pb
@@ -1,7 +1,7 @@
 
 C
 src/Anagramme.scala,e/8/e87386a36fe37da1c29b528766b89f2e347f4684
-Q
-!src/main/scala/revisionTP05.scala,8/c/8cd05f7ba3fc274185f9d48e3ba639b12ed0b34d
+U
+%src/main/scala/tmp/revisionTP05.scala,f/d/fd81ab51a634ba01ddad35f61c563936bd9a25f2
 Q
 !src/main/scala/revisionTP07.scala,5/7/575192c850b45827fed6b7551da67b9d7f7d4a7a
--- Master/M1/S1/ProgrammationMultiParadigmes/TP/WorkspaceScala/src/main/scala/revisionTP07.scala
+++ Master/M1/S1/ProgrammationMultiParadigmes/TP/WorkspaceScala/src/main/scala/revisionTP07.scala
@@ -1,13 +1,39 @@
 class Sudoku(startConfig_ : Array[Array[Int]]) {
   private val cote : Int = 9
+  def parcours_2(i_ : Int) : Unit = {
+    var i : Int = i_
+    if(i < 81) {
+      println(i + "->" + ((i / 9) + 1, (i % 9) + 1))
+      i += 1
+      return(parcours_2(i))
+    }
+  }
 
-  def solver(): List[Array[Array[Int]]] = {
-    var grid : Array[Array[Int]] = startConfig_.clone()
-
-    def isPossibleAt(number_ : Int, x_ : Int, y_ : Int) : Boolean = {
-      for(nb <- 0 to (cote - 1)) {
-        if(grid(nb)(y_) == number_) return false
+  def parcours_22(i_ : Int = 0) : Unit = {
+    (i_ / 9, i_ % 9) match {
+      case (8, 8) => println("Dernière case")
+      case (x, y) => {
+        println(x + " " + y)
+        parcours_22(i_ + 1)
       }
     }
+  }
+}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val table: Array[Array[Int]] = Array(
+      Array(5, 3, 0, 0, 7, 0, 0, 0, 0),
+      Array(6, 0, 0, 1, 9, 5, 0, 0, 0),
+      Array(0, 9, 8, 0, 0, 0, 0, 6, 0),
+      Array(8, 0, 0, 0, 6, 0, 0, 0, 3),
+      Array(4, 0, 0, 8, 0, 3, 0, 0, 1),
+      Array(7, 0, 0, 0, 2, 0, 0, 0, 6),
+      Array(0, 6, 0, 0, 0, 0, 2, 8, 0),
+      Array(0, 0, 0, 4, 1, 9, 0, 0, 5),
+      Array(0, 0, 0, 0, 8, 0, 0, 7, 9)
+    )
+    var sud : Sudoku = new Sudoku(table)
+    sud.parcours_22()
   }
 }

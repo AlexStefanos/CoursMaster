3dee3918c9d4dff046146c391ec57a0db2e3594b
AlexStefanos <alexandrestefanoss@gmail.com>
2023-05-24 00:10:39 +0200

    23/05/23 vfatigue

--- M1/S2/BigData/TP/TP10/sparktopships/SparkTop10Ships.java
+++ M1/S2/BigData/TP/TP10/sparktopships/SparkTop10Ships.java
@@ -1,6 +1,7 @@
 package sparktopships;
 
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.*;
 import java.util.regex.Pattern;
 import scala.Tuple2;
@@ -23,53 +24,39 @@ import aisubs.AISFormat;
 import aisubs.TupleWritable;
 
 public class SparkTop10Ships {
-    private static final Pattern SPACE = Pattern.compile("\\P{L}+");
-    public static final String DEFAULT_INPUT_PATH = "s3a://ubs-datasets/ais/clean/2021/part-r-00000";
-    static final String DEFAULT_OUTPUT_FOLDER = "output-spark-ais-top10";
-
     public static void main(String[] args) throws Exception {
-        String inputPath = (args.length > 0)? args[0] : DEFAULT_INPUT_PATH;
-        String outputFolder = (args.length > 1) ? args[1] : DEFAULT_OUTPUT_FOLDER;
 
-        SparkConf sparkConf = new JavaSpark().setMaster("yarn").setAppName("SparkTop10Ships");
+        SparkConf sparkConf = new SparkConf().setMaster("yarn").setAppName("SparkTop10Ships");
         JavaSparkContext sc = new JavaSparkContext(sparkConf);
-        JavaRDD<String> lines = sc.textFile();
-        JavaRDD<TupleWritable> ships = lines.flatMap(s -> Arrays.asList(SPACE.split(s)).iterator()).map(s -> s.trim());
+        JavaRDD<String> lines = sc.textFile("s3a://ubs-datasets/ais/clean/2021/part-r-00000");
+        JavaRDD<TupleWritable> ships = lines.map(s -> AISFormat.getFieldTuple(s));
 
         ships.persist(StorageLevel.MEMORY_ONLY());
         long ships_cnt = ships.count();
+        JavaPairRDD<String,Float> boatSpeeds = ships.mapToPair(s -> new Tuple2<String, Float>(
+            s.get(AISFields.MMSI.ordinal()),
+            Float.valueOf(s.get(AISFields.SOG.ordinal()))
+        ));
 
-        List<String> stop_words_list = sc.textFile("s3://ubs-datasets/ais/clean/2021/*.txt").collect();
-        HashSet<String> stop_words = new HashSet<>(stop_words_list);
-        Broadcast<HashSet<String>> stop_words_broadcast = sc.broadcast(stop_words);
+        JavaPairRDD<String, Float> reduce = boatSpeeds.reduceByKey((s1, s2) -> (s1 > s2 ? s1 : s2));
+        JavaPairRDD<String, Float> top = sc.parallelizePairs(reduce.top(10, new BoatSpeedComparator()));
 
-        LongAccumulator accepted_cnt = sc.sc().longAccumulator();
-        LongAccumulator rejected_cnt = sc.sc().longAccumulator();
-        JavaRDD<String> accepted = ships.filter(s -> {
-            if(stop_words_broadcast.value().contains(s)) {
-                rejected_cnt.add(1);
-                return(false);
-            } else {
-                accepted_cnt.add(1);
-                return(true);
-            }
-        });
-        JavaPairRDD<String, Integer> ones = accepted.mapToPair(s -> new Tuple2<>(s, 1)).filter("speed");
-        ones.limit(10).show();
-        JavaPairRDD<String, Integer> counts = ones.reduceByKey((i1, i2) -> (i1 + i2));
-
-        counts.persist(StorageLevel.MEMORY_ONLY());
-        long distinct_cnt = counts.count();
-        System.out.println("Number of distinct ships = " + distinct_cnt);
         Configuration hadoopConf = new Configuration();
         FileSystem hdfs = FileSystem.get(hadoopConf);
         Path output_path = new Path("output-spark-top-ships");
 
         hdfs.delete(output_path, true);
-        JavaPairRDD<Text, IntWritable> writables = counts.mapToPair(t -> new Tuple2<Text, IntWritable>(new Text(t._1()), new IntWritable(t._2())));
+        JavaPairRDD<Text, FloatWritable> writables = top.mapToPair(t -> new Tuple2<>(new Text(t._1()), new FloatWritable(t._2())));
         writables.saveAsTextFile("output-spark-top-ships");
         sc.close();
         sc.stop();
+    }
+
+    public static class BoatSpeedComparator implements Comparator<Tuple2<String, Float>>, Serializable {
+        @Override
+        public int compare(Tuple2<String, Float> boatSpeed1, Tuple2<String, Float> boatSpeed2) {
+            return(Float.compare(boatSpeed1._2(), boatSpeed2._2()));
+        }
     }
 }
 
